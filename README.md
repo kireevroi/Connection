47faef9e8d0f5f928ba902a0a75f62bd1a56d760
kireevroi <50318432+kireevroi@users.noreply.github.com>
2021-11-11 12:37:43 +0300

    Finished source code

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # connection
+A simple script to check if you can connect to a website or not.
+
+Just fill in the websites you want to check into websites.txt and then start!
+The results are displayed in status.csv as a table.
---
+++ connection.py
@@ -0,0 +1,25 @@
+import csv
+import requests
+
+list_status = []
+list_status.append(["Website", "Working"])
+
+def scrape():
+    with open("websites.txt", "r") as inquiry:
+        for i in inquiry:
+            website = i.strip()
+            status = requests.get(website).status_code
+            if status == 200:
+                list_status.append([website, "Working"])
+            else:
+                list_status.append([website, "Not Working"])
+    return list_status
+
+def export(export_list):
+    with open("status.csv", "w", newline = "") as export_write:
+        writing = csv.writer(export_write)
+        for i in export_list:
+            writing.writerow(i)
+
+
+export(scrape())
